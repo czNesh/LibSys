6e2bfb6c69faef6330d3fef5d2fd4856d7f17846
Petr Hejhal <nesh.cz@gmail.com>
2012-10-21 00:20:58 +0200

    Počátek projektu

---
+++ src/libsys/LibSys.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libsys;
+
+/**
+ *
+ * @author eXtreme
+ */
+public class LibSys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
